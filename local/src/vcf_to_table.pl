a5aa459680aa60e7c21857c1eabafe6358387ea5
Elena Grassi <grassi.e@gmail.com>
2019-11-08 15:24:07 +0100

    vcf_to_table minimally operating

--- local/src/vcf_to_table.pl
+++ local/src/vcf_to_table.pl
@@ -9,9 +9,9 @@ my $multi = $ARGV[1];
 my $nmulti = 0;
 while (<STDIN>) {
     chomp;
-    my @line = $_;
+    my @line = split("\t", $_);
     die "I expect a single sample vcf" if scalar(@line) != 10;
-    die "Wrong vcf FORMAT" if $line[0] ne "GT:AD:AF:DP:F1R2:F2R1";
+    die "Wrong vcf FORMAT" if ($line[8] ne 'GT:AD:AF:DP:F1R2:F2R1:SB' && $line[8] ne 'GT:AD:DP:GQ:PL');
     if ($line[4] =~ /,/) {
         if ($multi eq 'multi') {
             die "Sorry still to be implemented"; # probably will need to use a library for this
@@ -19,11 +19,17 @@ while (<STDIN>) {
         $nmulti++;
         next;     
     } else {
-        &manage_nomulti($line[2], $line[3], $line[4], $line[9], $what);
+        &manage_entry($line[2], $line[3], $line[4], $line[9], $what);
     }
 }
 
-sub manage_entry($id, $ref, $alt, $g, $what) {
+sub manage_entry {
+    my $id = shift;
+    my $ref = shift;
+    my $alt = shift;
+    my $g = shift;
+    my $what = shift;
+    #mutect:
     #GT:AD:AF:DP:F1R2:F2R1   0/1:14,4:0.235:18:7,2:7,2 
     ##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
     ##FORMAT=<ID=AF,Number=A,Type=Float,Description="Allele fractions of alternate alleles in the tumor">
@@ -32,14 +38,21 @@ sub manage_entry($id, $ref, $alt, $g, $what) {
     ##FORMAT=<ID=F2R1,Number=R,Type=Integer,Description="Count of reads in F2R1 pair orientation supporting each allele">
     ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
     ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+    #haplotypecaller:
+    #GT:AD:DP:GQ:PL  0/1:365,53:486:99:132,0,7982
+    ##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+    ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
+    ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+    ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+    # both good
     if ($what eq 'SNV') {
         return if (length($ref) != 1 || length($alt) != 1);
-    } elsif ($what eq 'SNV')
+    } elsif ($what eq 'indel') { 
         return if (length($ref) == 1 && length($alt) == 1);
     } # we do not check for both
-    @afs = split(":",$g);
+    my @afs = split(":",$g);
     my @nreads = split(',',$afs[1]);
-    print $id . "\t" . $afs[0] . "\t" . $nreads[0] . "\t" . $nreads[1] . "\t" . $afs[1] . "\n";
+    print $id . "\t" . $afs[0] . "\t" . $nreads[0] . "\t" . $nreads[1] . "\t" . $afs[2] . "\n";
 }
 
 
