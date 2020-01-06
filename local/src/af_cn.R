0003c42754ad3204ef812d7ddf4b9fcd54ca7f05
Elena Grassi <grassi.e@gmail.com>
2020-01-07 00:01:11 +0100

    AF CN plot

---
+++ local/src/af_cn.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+args <- commandArgs(trailingOnly = T)
+mutfile <- args[1]
+maxcn <- as.numeric(args[2])
+outputplot <- args[3]
+save.image("p.RData")
+
+muts <- read.table(gzfile(mutfile), header=FALSE, stringsAsFactors=FALSE)
+colnames(muts) <- c("chr","b","e","id")
+muts$af <- as.numeric(sapply(muts$id, function(x) { strsplit(x, ":")[[1]][7]}))
+muts$cn <- sapply(muts$id, function(x) { strsplit(x, ":")[[1]][8]})
+muts$cn <- as.numeric(muts$cn)
+muts <- muts[muts$cn <= maxcn,]
+muts$cn <- factor(muts$cn, levels=seq(1, maxcn))
+
+ggplot(data=muts, aes(x=af, color=cn)) + geom_density(position="identity")+theme_bw()
+ggsave(outputplot)
+
