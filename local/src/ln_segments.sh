8205f41beacae943db060660ca03ee0cf5592558
Elena Grassi <grassi.e@gmail.com>
2020-01-22 13:11:29 +0100

    Rule for all pairs

--- local/src/ln_segments.sh
+++ local/src/ln_segments.sh
@@ -1,5 +1,5 @@
 #(base) [CRC1307_clones_biod]egrassi@hactarlogin$ ln -s ../CRC1307_simul/CRC1307LMO_segments.txt CRC1307_02_0_segments.txt
-s=(CRC1307_08_0 CRC1307_09_0 CRC1307_02_1_A CRC1307_02_1_B CRC1307_02_1_E CRC1307_08_1_B CRC1307_08_1_D CRC1307_08_1_E CRC1307_09_1_B CRC1307_09_1_C CRC1307_09_1_E)  
+s=(CRC1307-02-0 CRC1307-08-0 CRC1307-09-0 CRC1307-02-1-A CRC1307-02-1-B CRC1307-02-1-E CRC1307-08-1-B CRC1307-08-1-D CRC1307-08-1-E CRC1307-09-1-B CRC1307-09-1-C CRC1307-09-1-E CRC1307-08-MA-A CRC1307-08-MA-C CRC1307-08-MA-F CRC1307-08-MC-D CRC1307-08-MC-E CRC1307-08-MC-F CRC1307-08-MI-A CRC1307-08-MI-B CRC1307-08-MI-F)  
 
 # are these segments with or without recalibration? without:
 #[egrassi@occam ~]>ls -al bit/prj/snakegatk/dataset/CRC1307/sequenza/CRC1307LMO/CRC1307LMO_segments.txt 
@@ -8,5 +8,5 @@ s=(CRC1307_08_0 CRC1307_09_0 CRC1307_02_1_A CRC1307_02_1_B CRC1307_02_1_E CRC130
 
 for item in ${s[*]}
 do
-    ln -s ../CRC1307_simul/CRC1307LMO_segments.txt ${item}_segments.txt
+    ln -s ../CRC1307_simul/CRC1307LMO_segments.txt ${item}.segments.txt
 done
