aaf34618ad8834bf9e3d56ef9c24bdb7de73a9a0
Elena Grassi <grassi.e@gmail.com>
2020-03-23 17:12:19 +0100

    dn/ds pipeline

---
+++ local/src/dnds.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+library(dndscv)
+
+args <- commandArgs(trailingOnly = T)
+infile <- args[1]
+outfile <- args[2]
+image <- args[3]
+rda <- args[4]
+
+getdnds <- function(file) {
+	data <- read.table(file, sep="\t", header=FALSE)
+	colnames(data) <- c('sampleID','chr', 'pos', 'ref', 'alt')
+	return(dndscv(data, refdb=rda, cv=NULL))
+}
+
+res <- getdnds(infile)
+write.table(res$globaldnds, file = outfile, sep="\t", quote=FALSE)
+save.image(image)
