cd31eb811887ccb97704176e7075e4025377e62b
Elena Grassi <grassi.e@gmail.com>
2020-10-27 16:43:48 +0100

    Automatic generation of multiple thr of coverage for bexp

---
+++ local/src/qc_bexp_plots.R
@@ -0,0 +1,60 @@
+d <- read.table("/scratch/trcanmed/AF_spectra/dataset/CRC1307_150x/mutect_nobin_100x/CRC1307-02-1-E.bexpcalls.gen.tsv.gz", sep="\t", header=T)
+head(d)
+library(ggplot2)
+dcn2 <- d[d$cn==2 & d$generation==4,]
+#af <- dcn2$af; excum <- sapply(1:length(af),function(i) sum(af[i] <= af[1:length(af)]))
+dim(dcn2)
+af <- dcn2$af
+excum <- sapply(1:length(af),function(i) sum(af[i] <= af[1:length(af)]))
+data <- data.frame(af=af, cum=excum)
+dim(data)
+#+ coord_trans(y = "log10")
+ggplot(data, aes(x=log10(af), y=log10(excum))) + geom_point()+geom_smooth(method=lm)+theme_bw()+xlab('log10(AF)')+ylab('Log10 Cumulative n. of muts > AF')
+head(dcn2)
+dsign <- dcn2[dcn2$pBH2 < 0.05 & dcn2$pBH1 < 0.05,]
+af <- dsign$af
+excum <- sapply(1:length(af),function(i) sum(af[i] <= af[1:length(af)]))
+data <- data.frame(af=af, cum=excum)
+dim(data)
+
+ggplot(data, aes(x=log10(af), y=log10(excum))) + geom_point()+theme_bw()+xlab('log10(AF)')+ylab('Log10 Cumulative n. of muts > AF')+geom_smooth(method=lm)
+
+# sottoriva
+ddd <- dsign
+exsubcl <- ddd$af
+excum <- sapply(1:length(exsubcl),function(i)sum(exsubcl[i]<=exsubcl[1:length(exsubcl)]))
+invf <- 1/exsubcl
+maxi <- length(invf)
+labels <-  c(1, floor(maxi/5), floor(maxi/2), floor(maxi/0.5), maxi)
+model <- lm(excum~invf)
+sfit <- summary(model)
+plot(invf, excum, cex=1.5, xaxt="n", xlab='1/f', ylab="Cumulative n. of muts M(f)", main=paste0("R2=", round(sfit$r.squared, digits=3)))
+oi <- invf[order(invf)]
+oex <- exsubcl[order(-exsubcl)]
+axis(1, at=oi[labels],labels=paste0("1/",oex[labels]), las=2)
+abline(model, col="red")
+
+
+## bonf
+dsign <- dcn2[dcn2$pBonf1 < 0.05 & dcn2$pBonf2 < 0.05,]
+af <- dsign$af
+excum <- sapply(1:length(af),function(i) sum(af[i] <= af[1:length(af)]))
+data <- data.frame(af=af, cum=excum)
+
+ggplot(data, aes(x=log10(af), y=log10(excum))) + geom_point()+theme_bw()+xlab('log10(AF)')+ylab('Log10 Cumulative n. of muts > AF')+geom_smooth(method=lm)
+
+# sottoriva
+ddd <- dsign
+exsubcl <- ddd$af
+excum <- sapply(1:length(exsubcl),function(i)sum(exsubcl[i]<=exsubcl[1:length(exsubcl)]))
+invf <- 1/exsubcl
+maxi <- length(invf)
+labels <-  c(1, floor(maxi/5), floor(maxi/2), floor(maxi/0.5), maxi)
+model <- lm(excum~invf)
+sfit <- summary(model)
+plot(invf, excum, cex=1.5, xaxt="n", xlab='1/f', ylab="Cumulative n. of muts M(f)", main=paste0("R2=", round(sfit$r.squared, digits=3)))
+oi <- invf[order(invf)]
+oex <- exsubcl[order(-exsubcl)]
+axis(1, at=oi[labels],labels=paste0("1/",oex[labels]), las=2)
+abline(model, col="red")
+
