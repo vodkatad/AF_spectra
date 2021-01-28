0217b0a95a6dd320bf9da70528142115850829a9
Elena Grassi <grassi.e@gmail.com>
2021-01-28 14:51:48 +0100

    Fixed vivo gained plots and vitro-vivo corr

--- local/src/gained_plot.R
+++ local/src/gained_plot.R
@@ -44,8 +44,12 @@ pdata$model <- rownames(pdata)
 
 if (!is.na(vivi) && vivi == "vivo") {
   our$model_clone <- paste0(our$model, "_", our$clone, "_", our$vivi)
+  scale_shape <- c(18,20,17)
+  legend_position <- 'right'
 } else {
   our$model_clone <- paste0(our$model, "_", our$clone)
+  scale_shape <- c(18,20)
+  legend_position <- 'none'
 }
 
 n <- length(levels(as.factor(our$model_clone)))
@@ -56,11 +60,11 @@ if (n <= length(cbPalette)) {
 ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
   geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+theme_bw()+ggtitle('Gained muts')+ylab(ylab)+
   geom_point(data=our, aes(x=model, y=gained, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
-  theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position="none",axis.title.y=element_text(size=15))+scale_color_manual(values=cbPalette)+scale_shape_manual(values=c(18,20))
+  theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position=legend_position,axis.title.y=element_text(size=15))+scale_color_manual(values=cbPalette)+scale_shape_manual(values=scale_shape)
 } else {
 ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
   geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+theme_bw()+ggtitle('Gained muts')+ylab(ylab)+
   geom_point(data=our, aes(x=model, y=gained, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
-  theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position="none",axis.title.y=element_text(size=15))+scale_shape_manual(values=c(18,20))
+  theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position=legend_position,axis.title.y=element_text(size=15))+scale_shape_manual(values=scale_shape)
 }
 ggsave(outfile)
