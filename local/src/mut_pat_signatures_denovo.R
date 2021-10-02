8f59bfa1c5d21276250eb8e1bb5405e43c72d11e
Elena Grassi <grassi.e@gmail.com>
2021-10-02 21:23:28 +0200

    MutPat denovo

--- local/src/mut_pat_signatures_denovo.R
+++ local/src/mut_pat_signatures_denovo.R
@@ -8,16 +8,20 @@ library(NMF)
 library(gridExtra)
 library(ggplot2)
 library(reshape)
-
+#library(RColorBrewer)
+#library(pheatmap)
 
 cosmic <- paste("https://cancer.sanger.ac.uk/cancergenome/assets/","signatures_probabilities.txt", sep = "") # ???
 
-setwd('/scratch/trcanmed/AF_spectra/dataset/')
 
-#files <- c("CRC0327-02.signin.vcf","CRC0327-04.signin.vcf","CRC0327-08.signin.vcf","CRC0441-01.signin.vcf","CRC0441-03.signin.vcf","CRC0441-10.signin.vcf","CRC1078-02.signin.vcf","CRC1078-07.signin.vcf","CRC1078-09.signin.vcf","CRC1307-02.signin.vcf","CRC1307-08.signin.vcf","CRC1307-09.signin.vcf","CRC1502-03.signin.vcf","CRC1502-08.signin.vcf","CRC1502-09.signin.vcf","CRC1502-10.signin.vcf","CRC1599LM-01.signin.vcf","CRC1599LM-03.signin.vcf","CRC1599LM-07.signin.vcf","CRC1599PR-01.signin.vcf","CRC1599PR-10.signin.vcf")
-#sample_names <- c("CRC0327-02","CRC0327-04","CRC0327-08","CRC0441-01","CRC0441-03","CRC0441-10","CRC1078-02","CRC1078-07","CRC1078-09","CRC1307-02","CRC1307-08","CRC1307-09","CRC1502-03","CRC1502-08","CRC1502-09","CRC1502-10","CRC1599LM-01","CRC1599LM-03","CRC1599LM-07","CRC1599PR-01","CRC1599PR-10")
-input <- 'mehh'
+args <- commandArgs(trailingOnly = TRUE)
+input <- args[1]
+outputdir <- args[2]
+palette <- args[3]
+nsign <- as.numeric(args[4])
+image <- args[5]
 
+# infile is tsv vfc / samplename_bulk|vivo|vitro
 vcfs <- read.table(input, sep="\t", header=FALSE, stringsAsFactors = FALSE)
 vcf_files <- vcfs$V1
 sample_names <- vcfs$V2
@@ -25,6 +29,8 @@ sample_names <- vcfs$V2
 vcfs <- read_vcfs_as_granges(vcf_files, sample_names, ref_genome)
 mut_mat <- mut_matrix(vcf_list = vcfs, ref_genome = ref_genome)
 
+setwd(outputdir)
+save.image(image)
 
 nrun_estimate <- 50
 nrun <- 150
@@ -33,11 +39,9 @@ mut_mat2 <- mut_mat + 0.0001
 estimate <- nmf(mut_mat2, rank=2:5, method="brunet", nrun=nrun_estimate, seed=seed)
 plot(estimate)
 
-nsign <- 3
 nmf_res <- extract_signatures(mut_mat, rank = nsign, nrun = nrun)
 
-base <- 'toplevel'
-names_sign <- paste0("sign_", base, "_", seq(1, nsign))
+names_sign <- paste0("sign_", seq(1, nsign))
 colnames(nmf_res$signatures) <- names_sign
 rownames(nmf_res$contribution) <- names_sign
 
@@ -90,9 +94,13 @@ mypc <- function (contribution, signatures, index = c(), coord_flip = FALSE, mod
   return(plot)
 }
 
+png('contrib_barplot.png')
 mypc(nmf_res$contribution, nmf_res$signature, mode = "relative")
+graphics.off()
 
+png('heatmap.png')
 plot_contribution_heatmap(nmf_res$contribution,sig_order = names_sign,cluster_samples=F)
+graphics.off()
 
 sp_url <- paste(cosmic, sep = "")
 cancer_signatures = read.table(sp_url, sep = "\t", header = TRUE)
@@ -104,18 +112,18 @@ cancer_signatures = cancer_signatures[as.vector(new_order),]
 row.names(cancer_signatures) = cancer_signatures$Somatic.Mutation.Type
 # Keep only 96 contributions of the signatures in matrix
 cancer_signatures = as.matrix(cancer_signatures[,4:33])
-hclust_cosmic = cluster_signatures(cancer_signatures, method = "average") # store signatures in new order
-cosmic_order = colnames(cancer_signatures)[hclust_cosmic$order]
-cos_sim_samples_signatures = cos_sim_matrix(mut_mat, cancer_signatures)
-plot_cosine_heatmap(cos_sim_samples_signatures,col_order = cosmic_order,cluster_rows = TRUE)
 
 us <- nmf_res$signatures
 colnames(us) <- names_sign
 hclust_cosmic_us = cluster_signatures(cbind(us, cancer_signatures), method = "average") # store signatures in new order
+
+png('hclust.png')
 plot(hclust_cosmic_us)
+graphics.off()
 
 # put together vitro-vivo of each starting clone and redo (to get more power in reconstructing signatures?)
 
 
 # 5 - 18 with k=2 and 1 sample for each starting clone - with k = 3 signature 8 pops out
 # 5 - 18 - 8 with k=3 and 1 sample for each model (vivo/vitro separated)
+save.image(image)
