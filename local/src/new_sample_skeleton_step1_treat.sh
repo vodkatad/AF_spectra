80161d0608cbf5d8be71d81fdd8e36ebc7cb16bf
Elena Grassi <grassi.e@gmail.com>
2022-09-28 21:46:17 +0200

    conf and sh helper for MA treats

---
+++ local/src/new_sample_skeleton_step1_treat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir $1
+mkdir $1/platypus_nobin_indels $1/sequenza $1/mutect_nobin $1/platypus_nobin $1/MutationalPatterns
+cd $1/platypus_nobin
+ln -s ../../../local/share/snakerule/Snakefile_clones_platypus_polished_nobinomial Snakefile
+cp ../../../local/share/snakemake/conf_baseline_CRC1430_mutect_platypus_polished.sk ../../../local/share/snakemake/conf_$1_mutect_platypus_polished.sk
+ln -s ../../../local/share/snakemake/conf_$1_mutect_platypus_polished.sk conf.sk
+cd ../platypus_nobin_indels/
+ln -s ../../../local/share/snakerule/Snakefile_clones_platypus_polished_nobinomial Snakefile
+ln -s ../../../local/share/snakemake/conf_$1_mutect_platypus_polished_indels.sk conf.sk
+cd ../mutect_nobin/
+ln -s ../../../local/share/snakerule/Snakefile_clones_real_segments Snakefile
+ln -s ../../../local/share/snakemake/conf_$1_mutect_platypus_polished.sk conf.sk
+cd ../sequenza
+ln -s ../../../local/share/snakerule/Snakefile_sequenza Snakefile
+ln -s ../../../local/share/snakemake/conf_$1_mutect_platypus_polished.sk conf.sk
+cd ../MutationalPatterns/
+ln -s ../../../local/share/snakerule/Snakefile_mutationalpatterns Snakefile
+ln -s ../../../local/share/snakemake/conf_$1_mutect_platypus_polished.sk conf.sk
+
+echo "Now add your samples info in ../local/share/snakemake/conf_$1_mutect_platypus_polished.sk, create ../local/share/snakemake/conf_$1_mutect_platypus_polished_indels.sk for indels - then call ../local/src/new_sample_snakes.sh"
