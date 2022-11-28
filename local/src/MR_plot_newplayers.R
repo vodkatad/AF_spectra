be8ae9084111236c7c0a3f0ad839651ca7eaf4cd
Elena Grassi <grassi.e@gmail.com>
2022-11-28 13:46:39 +0100

    Mixed old code + pseudopilep with qualities and for 30x

---
+++ local/src/MR_plot_newplayers.R
@@ -0,0 +1,127 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+
+args <- commandArgs(trailingOnly = T)
+infile <- args[1]
+outfile <- args[2]
+colors <- args[3]
+ggtheme <- args[4]
+load(ggtheme)
+
+our <- read.table(infile, sep="\t", header=FALSE, stringsAsFactors=FALSE)
+colnames(our) <- c('sample','MR_edu')
+our$MR <- our$MR_edu / 0.000000001
+our$model <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][1]; return(y[1])})
+our$clone <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][2]; return(y[1])})
+our$clone2 <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][4]; return(y[1])})
+
+confidence_interval <- function(vector, interval) {
+  # Standard deviation of sample
+  vec_sd <- sd(vector)
+  # Sample size
+  n <- length(vector)
+  # Mean of sample
+  vec_mean <- mean(vector)
+  # Error according to t distribution
+  error <- qt((interval + 1)/2, df = n - 1) * vec_sd / sqrt(n)
+  # Confidence interval as a vector
+  result <- c("lower" = vec_mean - error, "upper" = vec_mean + error, "mean" = vec_mean)
+  return(result)
+}
+
+LEVEL <- 0.99
+ic_clones <- sapply(unique(our$model), function(x) { confidence_interval(our[our$model==x,'MR'], LEVEL) })
+colnames(ic_clones) <- unique(our$model)
+pdata <- as.data.frame(t(ic_clones))
+pdata$model <- rownames(pdata)
+ggplot(pdata, aes(x=model, y=mean, color=model)) +  geom_point(position=position_dodge(1), stat="identity") +
+geom_errorbar(aes(ymin=lower, ymax=upper), width=.2, size=1, position=position_dodge(1))+theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')
+
+our$model_clone <- paste0(our$model, "_", our$clone)
+
+#cbPalette <- c("#ff5733", "#ff7433","#ff8d33", 
+#              #"#9d01fc","#a91efe", "#be52ff",
+#              "#f607b9","#fb49ce","#f998e0",
+#              "#155d00","#239203","#2fc603",
+#              "#77a003","#95c805","#bcfc08",
+#              "#0829fc","#4a62fb","#95a3fd"
+#              )
+n <- length(levels(as.factor(our$model_clone)))
+cbPalette <- unlist(strsplit(colors, ','))
+#if (n == length(cbPalette)) {
+# ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+# geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+
+#   geom_point(data=our, aes(x=model, y=MR, color=model_clone), stat="identity", shape=18, size=4, position=position_dodge(0.2))+
+#   theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position="none", axis.title.y=element_text(size=15))+scale_color_manual(values=cbPalette)
+# } else {
+# ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+# geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+
+#   geom_point(data=our, aes(x=model, y=MR, color=model_clone), stat="identity", shape=18, size=4, position=position_dodge(0.2))+
+#   theme(axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), legend.position="none", axis.title.y=element_text(size=15))
+# }
+
+# ggsave(outfile)
+
+# shape clones
+our$time <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][3]; return(y[1])})
+
+
+ctheme <- theme_bw()+theme(text=element_text(size=10), axis.text.x = element_text(size=15, angle=90, vjust=0.5, hjust=1), 
+                axis.title.y=element_text(size=20), axis.text.y=element_text(size=15), 
+                plot.title = element_text(face = "bold", size = 20, hjust = 0.5), legend.position='none'
+)
+
+if (n == length(cbPalette)) {
+  ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+    theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+    geom_point(data=our, aes(x=model, y=MR, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
+    ctheme+scale_color_manual(values=cbPalette)+scale_shape_manual(values=c(18,20))
+} else {
+  ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+    theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+    geom_point(data=our, aes(x=model, y=MR, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
+    ctheme+scale_shape_manual(values=c(18,20))
+}
+ggsave(outfile)
+
+save.image(paste0(outfile, '.Rdata'))
+
+#q('no')
+### Reorderered svg for Andrea with theme
+basename <- substr(outfile, 1, nchar(outfile)-4)
+reordered <- paste0(basename, "_reordered.png")
+new_order <- c('CRC0282', 'CRC0327', 'CRC0441', 'CRC1502', 'CRC1599PR', 'CRC1599LM', 'CRC1078', 'CRC1307', 'CRC1430', 'CRC1620')
+if (n == length(cbPalette)) {
+  # We need to keep colors in track, we do this adding it to our before setting factors.
+  ordered <- our$model_clone[order(our$model_clone)]
+  palette_df <- data.frame(palette=cbPalette, model_clone=unique(ordered), stringsAsFactors=FALSE)# unique keeps the order, gasp
+  #our <- merge(our, palette_df, by="model_clone")
+  our$model <- factor(our$model, levels=new_order)
+  pdata$model <- factor(pdata$model, levels=new_order)
+  palette_values <- c(palette_df$palette)
+  names(palette_values) <- palette_df$model_clone
+  ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+    theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+    geom_point(data=our, aes(x=model, y=MR, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
+    unmute_theme+ctheme+scale_color_manual(values=palette_values)+scale_shape_manual(values=c(18,20))
+} else {
+  ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+    theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+    geom_point(data=our, aes(x=model, y=MR, color=model_clone, shape=time), stat="identity", shape=18, size=4, position=position_dodge(0.2))+
+    unmute_theme+ctheme+scale_shape_manual(values=c(18,20))
+}
+ggsave(reordered)
+
+
+library(ggsignif)
+
+
+ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=3) +
+  geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+  geom_point(data=our, aes(x=model, y=MR, color=model_clone, shape=time), stat="identity", size=4, position=position_dodge(0.2))+
+  ctheme+scale_color_manual(values=cbPalette)+scale_shape_manual(values=c(18,20))+
+  geom_signif(data=our, mapping=aes(x=model, y=MR), 
+              comparisons = list(c("CRC0282", "CRC1307"),c('CRC1307','CRC1599PR')), map_signif_level=TRUE)
+
+ggsave('MR_edu_SNV_ggsignif.pdf')
--- local/src/meh.R
+++ local/src/meh.R
@@ -1,13 +1,61 @@
-load('/scratch/trcanmed/AF_spectra/dataset/meh.png.Rdata')
+load('/scratch/trcanmed/AF_spectra/dataset/meh2.png.Rdata') # edu
 our$treat <- ifelse(grepl('N', our$clone2), 'NT', 'Afatinib')
 our$mt <- paste0(our$model, our$treat)
+our$treat <- factor(our$treat, levels=c('NT', 'Afatinib'))
+our$mt <- factor(our$mt, levels=c('CRC1430NT', 'CRC1430Afatinib','CRC1620NT', 'CRC1620Afatinib'))
+
 #ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
-geom_point(data=our, aes(x=mt, y=MR, color=treat), stat="identity", size=2, position=position_dodge(0.2))
-ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
-geom_point(data=our, aes(x=mt, y=MR, color=treat), stat="identity", size=2, position=position_dodge(0.2))
-ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
-geom_point(data=our, aes(x=mt, y=MR, color=treat), stat="identity", size=2
-)
+ggplot(our, aes(x=mt, y=MR, color=model_clone)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+  geom_point(stat="identity", size=2)+
+  geom_line(aes(group=clone))+
+  scale_color_manual(values=c('#5efeef','#01e4ce','#b3fff7','#7c7c7c','#666666','#cccccc'))+ scale_x_discrete(labels=c('CRC1430NT'="NT", 'CRC1430Afatinib'="Afatinib",
+                                                                               'CRC1620NT'="NT", 'CRC1620Afatinib'="Afatinib"))
+
+
+load('/scratch/trcanmed/AF_spectra/dataset/meh.png.Rdata') # conte
+our$treat <- ifelse(grepl('N', our$clone2), 'NT', 'Afatinib')
+our$mt <- paste0(our$model, our$treat)
+our$treat <- factor(our$treat, levels=c('NT', 'Afatinib'))
+our$mt <- factor(our$mt, levels=c('CRC1430NT', 'CRC1430Afatinib','CRC1620NT', 'CRC1620Afatinib'))
+
+#ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+ggplot(our, aes(x=mt, y=MR, color=model_clone)) +theme_bw()+ggtitle('MR cell counts')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+  geom_point(stat="identity", size=2)+
+  geom_line(aes(group=clone))+
+  scale_color_manual(values=c('#5efeef','#01e4ce','#b3fff7','#7c7c7c','#666666','#cccccc'))+ scale_x_discrete(labels=c('CRC1430NT'="NT", 'CRC1430Afatinib'="Afatinib",
+                                                                                                                       'CRC1620NT'="NT", 'CRC1620Afatinib'="Afatinib"))
+
+
+
+
+load('/scratch/trcanmed/AF_spectra/dataset/meh3.png.Rdata') # n muts
+our$treat <- ifelse(grepl('N', our$clone2), 'NT', 'Afatinib')
+our$mt <- paste0(our$model, our$treat)
+our$treat <- factor(our$treat, levels=c('NT', 'Afatinib'))
+our$mt <- factor(our$mt, levels=c('CRC1430NT', 'CRC1430Afatinib','CRC1620NT', 'CRC1620Afatinib'))
+#ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+ggplot(our, aes(x=mt, y=MR_edu, color=model_clone)) +theme_bw()+ggtitle('Accumulated muts')+ylab('muts')+xlab('')+
+  geom_point(stat="identity", size=2)+
+  geom_line(aes(group=clone))+
+  scale_color_manual(values=c('#5efeef','#01e4ce','#b3fff7','#7c7c7c','#666666','#cccccc'))+ scale_x_discrete(labels=c('CRC1430NT'="NT", 'CRC1430Afatinib'="Afatinib",
+                                                                                                                       'CRC1620NT'="NT", 'CRC1620Afatinib'="Afatinib"))
+
+
+
+load('/scratch/trcanmed/AF_spectra/dataset/meh4.png.Rdata') # normalized muts
+our$treat <- ifelse(grepl('N', our$clone2), 'NT', 'Afatinib')
+our$mt <- paste0(our$model, our$treat)
+our$treat <- factor(our$treat, levels=c('NT', 'Afatinib'))
+our$mt <- factor(our$mt, levels=c('CRC1430NT', 'CRC1430Afatinib','CRC1620NT', 'CRC1620Afatinib'))
+our$MR <- our$MR_edu * 1000000
+#ggplot(pdata, aes(x=mt)) +theme_bw()+ggtitle('MR EDU')+ylab('MR, mut/(division*bp) *10^-9')+xlab('')+
+ggplot(our, aes(x=mt, y=MR, color=model_clone)) +theme_bw()+ggtitle('Accumulated muts')+ylab('muts/Mbp')+xlab('')+
+  geom_point(stat="identity", size=2)+
+  geom_line(aes(group=clone))+
+  scale_color_manual(values=c('#5efeef','#01e4ce','#b3fff7','#7c7c7c','#666666','#cccccc'))+ scale_x_discrete(labels=c('CRC1430NT'="NT", 'CRC1430Afatinib'="Afatinib",
+                                                                                                                       'CRC1620NT'="NT", 'CRC1620Afatinib'="Afatinib"))
+
+
 
 
 1867  cut -f 2,7 */platypus_nobin/all.MR_baseline_ov | grep -v end > meh
@@ -26,3 +74,49 @@ cp NT_CRC1620/platypus_nobin/all.MR_baseline_ov paperino
 egrassi@godot:/scratch/trcanmed/AF_spectra/dataset$ cat NT_CRC1430/platypus_nobin/dnds.tsv T_CRC1430/platypus_nobin/dnds.tsv NT_CRC1620/platypus_nobin/dnds.tsv  T_CRC1620/platypus_nobin/dnds.tsv | grep -v name | grep wall > paperoga_dnds.tsv
 egrassi@godot:/scratch/trcanmed/AF_spectra/dataset$ vi paperoga_dnds.tsv 
 egrassi@godot:/scratch/trcanmed/AF_spectra/dataset$ /scratch/trcanmed/AF_spectra/local/bin/dnds_plot_overall paperoga_dnds.tsv paperoga_dnds.png  '#5efeef,#2b847c,#7c7c7c,#323232'
+
+
+#egrassi@godot:/scratch/trcanmed/AF_spectra/dataset$ cut -f 2,5 */platypus_nobin/all.MR_baseline_ov | grep -v end > meh3
+#egrassi@godot:/scratch/trcanmed/AF_spectra/dataset$ cut -f 2,5,6 */platypus_nobin/all.MR_baseline_ov | grep -v end | bawk '{print $1,$2/$3}' > meh4
+
+# mut
+/scratch/trcanmed/AF_spectra/local/bin/MR_plot meh3 meh3.png '#cc3300,#ff4000,#ff6633,#f607b9,#fb49ce,#f998e0,#9900ff,#ad33ff,#c266ff,#155d00,#239203,#2fc603,#239203,#77a003,#95c805,#95c805,#bcfc08,#bcfc08,#0829fc,#0829fc,#4a62fb,#4a62fb,#95a3fd,#95a3fd,#003399,#003399,#ff9900,#ffad33,#ff9900,#ffff00,#ffff66' /mnt/trcanmed/snaketree/prj/biobanca/dataset/V1/theme_20.Rdata
+
+
+# normaliz mut
+/scratch/trcanmed/AF_spectra/local/bin/MR_plot meh4 meh4.png '#cc3300,#ff4000,#ff6633,#f607b9,#fb49ce,#f998e0,#9900ff,#ad33ff,#c266ff,#155d00,#239203,#2fc603,#239203,#77a003,#95c805,#95c805,#bcfc08,#bcfc08,#0829fc,#0829fc,#4a62fb,#4a62fb,#95a3fd,#95a3fd,#003399,#003399,#ff9900,#ffad33,#ff9900,#ffff00,#ffff66' /mnt/trcanmed/snaketree/prj/biobanca/dataset/V1/theme_20.Rdata
+
+
+load('/mnt/trcanmed/snaketree/prj/snakegatk/dataset/Pri_Mets_godot/CRC1599PRX0A02002TUMD03000V2.fit.0.025_0.2.pdf.debug.RData')
+##ff9900,#ffff00
+hist(exsubcl_nohigh, breaks=50, cex=1.5, xlab="Allelic frequency (f)", ylab="Number of muts", border="black", col='#ff9900', main="", ylim=c(0,30))
+
+dev.off()
+load('/mnt/trcanmed/snaketree/prj/snakegatk/dataset/Pri_Mets_godot/CRC1599LMX0A02001TUMD03000V2.fit.0.025_0.2.pdf.debug.RData')
+##ff9900,#ffff00
+hist(exsubcl_nohigh, breaks=50, cex=1.5, xlab="Allelic frequency (f)", ylab="Number of muts", border="black", col='#ffff00', main="")
+
+setwd('/mnt/trcanmed/snaketree/prj/snakegatk/dataset/Pri_Mets_godot/')
+
+w <- c('CRC0053LMX0A02204TUMD07000V2.fit.0.12_0.24.pdf.debug.RData','CRC0065PRX0A01201TUMD03000V2.fit.0.12_0.24.pdf.debug.RData', 'CRC1387LMX0A02001TUMD03000V2.fit.0.12_0.24.pdf.debug.RData',
+       'CRC1473PRX0B01001TUMD03000V2.fit.0.12_0.24.pdf.debug.RData', 'CRC0053PRX0A01201TUMD05000V2.fit.0.12_0.24.pdf.debug.RData','CRC1331LMX0A02005TUMD03000V2.fit.0.12_0.24.pdf.debug.RData',
+       'CRC1387PRX0B01001TUMD05000V2.fit.0.12_0.24.pdf.debug.RData','CRC1599LMX0A02001TUMD03000V2.fit.0.12_0.24.pdf.debug.RData', 'CRC0065LMX0B02205TUMD02000V2.fit.0.12_0.24.pdf.debug.RData', 
+       'CRC1331PRX0A01001TUMD05000V2.fit.0.12_0.24.pdf.debug.RData','CRC1473LMX0B02002TUMD03000V2.fit.0.12_0.24.pdf.debug.RData')
+w <- w[order(w)]
+i <- 0
+xlim <- c(30, 30, 40, 40, 50, 50, 50, 50, 40, 40)
+j <- 1
+for (rd in w) {
+  if (i == 0) {
+    col = '#595959'
+    i = 1
+  } else {
+    col = '#adacac'
+    i = 0
+  }
+  load(rd)
+  model <- substr(rd, 0, 10)
+  hist(exsubcl_nohigh, breaks=50, cex=1.5, xlab="Allelic frequency (f)", ylab="Number of muts", border="black", col=col, ylim=c(0,lim[j]), main=model)
+  j <- j + 1
+}
+
