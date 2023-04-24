6736927d0d2dcdcef579f6f0a6129c62effdc7b2
Elena Grassi <grassi.e@gmail.com>
2023-04-24 17:05:30 +0200

    COSMIC signatures fit log and tsv.
    N. samples seems fine!

--- local/src/mut_pat_signatures_fit.R
+++ local/src/mut_pat_signatures_fit.R
@@ -16,15 +16,31 @@ input <- args[1]
 outputheat <- args[2]
 outputcosine <- args[3]
 palette <- args[4]
+log_f <- args[5]
+outputheattsv <- args[6]
+outputcosinetsv <- args[7]
 
 # infile is tsv vfc / samplename_bulk|vivo|vitro
 vcfs <- read.table(input, sep="\t", header=FALSE, stringsAsFactors = FALSE)
 vcf_files <- vcfs$V1
 sample_names <- vcfs$V2
 
+# I wanted to use expand in the rule that generates signinput_..., therefore single samples
+# will create empty vcf if they do not have any in vitro/in vivo clone and we need to get rid of them here
+keep_noempty <- file.size(vcf_files) != 0L
+vcf_files <- vcf_files[keep_noempty]
+sample_names <- sample_names[keep_noempty]
+
+sink(log_f)
+print("N samples:")
+print(length(vcf_files))
+table((unlist(lapply(strsplit(sample_names,'_'), function(x){ x[length(x)] }))))
+print("2nd round")
+print(table(grepl('2nd_', sample_names)))
+sink()
+
 vcfs <- read_vcfs_as_granges(vcf_files, sample_names, ref_genome)
 mut_mat <- mut_matrix(vcf_list = vcfs, ref_genome = ref_genome)
-
 
 ### cosmic
 cosmic <- paste("https://cancer.sanger.ac.uk/cancergenome/assets/","signatures_probabilities.txt", sep = "") # ???
@@ -39,17 +55,10 @@ row.names(ref_signatures) = ref_signatures$Somatic.Mutation.Type
 # Keep only 96 contributions of the signatures in matrix
 ref_signatures = as.matrix(ref_signatures[,4:33])
 ##
-
-## signal TODO  new version of package has it already inside?
-## other cosmic version
-###
-
-
-
 ff <- fit_to_signatures(mut_mat, ref_signatures)
 #ff$contribution
 #which(rowSums(ff$contribution) > 10)
-select <- which(rowSums(ff$contribution) > 10)
+#select <- which(rowSums(ff$contribution) > 10)
 #plot_contribution_heatmap(ff$contribution,cluster_samples = TRUE,method = "complete")
 
 
@@ -67,6 +76,7 @@ ggplot(cos_sim_ori_rec, aes(y=cos_sim, x=reorder(s, n), fill=sample)) +
   geom_hline(aes(yintercept=.95)) +scale_fill_brewer(palette = "Dark2")
 
 ggsave(outputcosine)
+write.table(cos_sim_ori_rec, file=outputcosinetsv, sep="\t", quote=FALSE)
 
 data <- as.matrix(ff$contribution)
 #select <- which(rowSums(data) > 10)
@@ -91,6 +101,8 @@ cbPalette2 <- unlist(strsplit(palette, ','))
 names(cbPalette2) <- levels(annot_rows$model)
 annot_colors <- list(sample=col, model=cbPalette2)
 pheatmap(data, fontsize_row = 9, fontsize_col=9, show_colnames = TRUE,  cluster_cols=FALSE,  annotation_row=annot_rows, annotation_colors = annot_colors,  color=brewer.pal(9, 'PuBu'), filename=outputheat)
+
+write.table(data, file=outputheattsv, sep="\t", quote=FALSE)
 
 ############# manual AIRC
 save.image('sign.Rdata')
