3924fdcd81df51d265e0cb7318719578ac698432
Elena Grassi <grassi.e@gmail.com>
2023-05-08 11:44:30 +0200

    Figure 3

--- local/src/fig3a_MR.R
+++ local/src/fig3a_MR.R
@@ -20,13 +20,17 @@ names(pal) <- palette_df$model
 our <- read.table(MR_f, sep="\t", header=FALSE, stringsAsFactors=FALSE)
 
 colnames(our) <- c('sample','MR_edu')
-our <- our[grepl('-1-', our$sample),] # only 1st round
 
 our$MR <- our$MR_edu / 0.000000001
 our$model <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][1]; return(y[1])})
 our$clone <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][2]; return(y[1])})
 our$clone2 <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][4]; return(y[1])})
 our$model_clone <- paste0(our$model, "_", our$clone)
+our$time <- sapply(our$sample, function(x) {y<-strsplit(x, '-')[[1]][3]; return(y[1])})
+
+# we keep only models with at least one t2
+models_2nd_round <- unique(our[our$time==2, 'model'])
+our <- our[our$model %in% models_2nd_round,]
 
 our$model <- paste0(our$model, ifelse(!grepl('\\d$', our$model), '', ifelse(our$model=="CRC0282", 'PR', 'LM')))
 
@@ -45,46 +49,35 @@ confidence_interval <- function(vector, interval) {
   return(result)
 }
 
+# we compute avg on model_time
+our$model_time <- paste0(our$model, "_", our$time)
 LEVEL <- 0.99
-ic_clones <- sapply(unique(our$model), function(x) { confidence_interval(our[our$model==x,'MR'], LEVEL) })
-colnames(ic_clones) <- unique(our$model)
+ic_clones <- sapply(unique(our$model_time), function(x) { confidence_interval(our[our$model_time==x,'MR'], LEVEL) })
+colnames(ic_clones) <- unique(our$model_time)
 pdata <- as.data.frame(t(ic_clones))
-pdata$model <- rownames(pdata)
+pdata$model_time <- rownames(pdata)
+pdata$model <- sapply(pdata$model_time, function(x) {y<-strsplit(x, '_')[[1]][1]; return(y[1])})
+pdata$time <- sapply(pdata$model_time, function(x) {y<-strsplit(x, '_')[[1]][2]; return(y[1])})
+
 
 orderdf <- read.table(order_f, sep="\t", quote="", header=TRUE, stringsAsFactor=TRUE)
+orderdf$mean <- NULL
+orderdf$lower <- NULL
+orderdf$upper <- NULL
 orderdf$model <- paste0(orderdf$model, ifelse(!grepl('\\d$', orderdf$model), '', ifelse(orderdf$model=="CRC0282", 'PR', 'LM')))
-our$model <- factor(our$model, levels=orderdf$model)
-pdata$model <- factor(pdata$model, levels=orderdf$model)
+orderdf$x_ord <- seq(1, nrow(orderdf))
+our <- merge(our, orderdf, by="model")
+pdata <- merge(pdata, orderdf, by="model")
+our$ord_x_time <- our$x_ord + 1/3*as.numeric(our$time)
+pdata$ord_x_time <- pdata$x_ord + 1/3*as.numeric(pdata$time)
 
-p <- ggplot(pdata, aes(x=model, y=mean)) +  geom_point(stat="identity", shape=1, size=2) +
-  geom_segment(aes(y=lower, yend=upper, x=model, xend=model), size=0.6)+ylab('MR')+xlab('')+
-  geom_point(data=our, aes(x=model, y=MR, color=model_clone), stat="identity", size=2, shape=18, position=position_dodge(0.5))+
-  scale_color_manual(values=pal)+theme(legend.position="none", axis.text.x = element_blank())+unmute_theme
+our$model_time <- as.factor(our$model_time)
+p <- ggplot(pdata, aes(x=reorder(model_time, ord_x_time), y=mean)) +  geom_point(stat="identity", shape=1, size=2) +
+  geom_segment(aes(y=lower, yend=upper, x=reorder(model_time, ord_x_time), xend=reorder(model_time, ord_x_time)), size=0.6)+ylab('MR')+xlab('')+
+  geom_point(data=our, aes(x=reorder(model_time, ord_x_time), y=MR, color=model_clone), stat="identity", size=2, shape=18, position=position_dodge(0.5))+
+  scale_color_manual(values=pal)+unmute_theme+theme(legend.position="none", axis.text.x = element_blank())
 
-ggsave(outplot, plot=p, width=89, height=56, units="mm")
+ggsave(outplot, plot=p, width=89, height=89, units="mm")
 write.table(pdata, file=data_f, sep="\t", quote=FALSE)
 
-sink(log_f)
-print('n clones')
-print(nrow(our))
-# fold change + wilcox CRC0282, CRC1307
-m1 <- "CRC0282PR"
-m2 <- "CRC1307LM"
-print(paste0(m1, ' vs ', m2))
-op <- pdata[order(pdata$mean),]
-print(as.character(op[nrow(op), 'model']))
-print(as.character(op[nrow(op)-1, 'model']))
-print(pdata[pdata$model==m1, 'mean']/pdata[pdata$model==m2, 'mean'])
-print(wilcox.test(our[our$model==m1, 'MR'], our[our$model==m2, 'MR']))
-
-# fold change + wilcox CRC1599PR CRC1307
-m1 <- "CRC1307LM"
-m2 <- "CRC1599PR"
-print(paste0(m1, ' vs ', m2))
-print(as.character(op[1, 'model']))
-print(as.character(op[nrow(op)-1, 'model']))
-print(pdata[pdata$model==m1, 'mean']/pdata[pdata$model==m2, 'mean'])
-print(wilcox.test(our[our$model==m1, 'MR'], our[our$model==m2, 'MR']))
-
-sink()
 save.image(paste0(outplot, '.Rdata'))
--- local/src/fig3b_wilcox.R
+++ local/src/fig3b_wilcox.R
@@ -12,19 +12,19 @@ muts_tcga <- readRDS(tcga_rds)
 muts_wes <- readRDS(wes_rds)
 
 tcga_p <- ggplot(muts_tcga, aes_string(x='status', y='burden', fill='status'))+
-  geom_boxplot(outlier.shape = NA)+geom_jitter(size=0.15)+
+  geom_boxplot(outlier.shape = NA)+geom_jitter(size=0.15, color="gray34", alpha=0.8)+
   scale_fill_manual(values=c("darkgoldenrod","darkgreen"))+
   geom_signif(comparisons=list(c('MUT','WT')), size=0.2, textsize=1)+xlab('DNAH5')+ggtitle('TCGA')+
   unmute_theme
 
 wes_p <- ggplot(muts_wes, aes_string(x='status', y='burden', fill='status'))+
-  geom_boxplot(outlier.shape = NA)+geom_jitter(size=0.15)+
+  geom_boxplot(outlier.shape = NA)+geom_jitter(size=0.15, color="gray34", alpha=0.8)+
   scale_fill_manual(values=c("darkgoldenrod","darkgreen"))+
-  geom_signif(comparisons=list(c('MUT','WT')), size=0.2, textsize=1)+xlab('DNAH5')+ggtitle('vWES')+
+  geom_signif(comparisons=list(c('MUT','WT')), size=0.2, textsize=1)+xlab('DNAH5')+ggtitle('PDX WES')+
   unmute_theme
 
 p <- ggarrange(tcga_p, wes_p,  common.legend = TRUE)
 
 
-ggsave(outplot, plot=p, width=89, height=44, units="mm")
+ggsave(outplot, plot=p, width=89, height=89, units="mm")
 save.image(paste0(outplot, '.Rdata'))
