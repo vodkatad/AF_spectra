e97367ff0561c121420532a2e33b21518b3e6149
Elena Grassi <grassi.e@gmail.com>
2023-08-03 14:51:26 +0200

    Msigact and mpileup processing for MCL

--- local/src/clonal_Simo.R
+++ local/src/clonal_Simo.R
@@ -9,17 +9,19 @@ pal2 <- palette_df$palette
 names(pal2) <- palette_df$model_clone
 
 subclonal <- read.table('vitro_gained_binSNV', header=FALSE, sep="\t", stringsAsFactors=FALSE)
+#subclonal <- read.table('MR_edu_binSNV', header=FALSE, sep="\t", stringsAsFactors=FALSE)
 colnames(subclonal) <- c('sample', 'MR')
-#subclonal$MR_edu_cl <- subclonal$MR / 0.000000001
 subclonal$MR_edu_cl <- subclonal$MR 
+#subclonal$MR_edu_cl <- subclonal$MR / 0.000000001
 MR <- read.table('vitro_gained_SNV', header=FALSE, sep="\t", stringsAsFactors=FALSE)
+#MR <- read.table('MR_edu_SNV', header=FALSE, sep="\t", stringsAsFactors=FALSE)
 colnames(MR) <- c('sample', 'MR')
-#MR$MR_edu <- MR$MR / 0.000000001
 MR$MR_edu <- MR$MR 
+#MR$MR_edu <- MR$MR / 0.000000001
 
 merged <- merge(subclonal, MR, by='sample')
 merged$smodel <- substr(merged$sample, 0, 7)
-#wanted <- c('CRC1599', 'CRC1307', 'CRC1078')
+wanted <- c('CRC1599', 'CRC1307', 'CRC1078')
 merged <- merged[merged$smodel != 'CRC0282',]
 
 merged$model <- sapply(merged$sample, function(x) {y<-strsplit(x, '-')[[1]][1]; return(y[1])})
@@ -73,4 +75,58 @@ ggplot() +
   geom_errorbar(data=pdata, aes(x=mean.x, y=mean.y, ymin=lower.y, ymax=upper.y), width=.05, size=.2)+
   geom_errorbar(data=pdata, aes(x=mean.x, y=mean.y, xmin=lower.x, xmax=upper.x), width=.05, size=.2)+
   scale_fill_manual(values=pal2)+scale_color_manual(values=pal, guide='none')+
-  xlab('# SNVs gained')+ylab('# clonal SNVs gained')+theme(legend.position="right")
+  #xlab('# SNVs gained')+ylab('# clonal SNVs gained')+theme(legend.position="right")+geom_abline(slope=1, intercept=0)
+  xlab('MR')+ylab('clonal MR')+theme(legend.position="right")+geom_abline(slope=1, intercept=0)
+
+
+### n bulk muts on CN1-2-3
+d <- read.table(gzfile('/scratch/trcanmed/AF_spectra/datasetV2/CRC1307/tree/bulk.var_cnv.tsv.gz'), sep="\t", header=F, stringsAsFactors = F)
+colnames(d) <- c('chr', 'b', 'e', 'info')
+
+infos <- strsplit(d$info, split=':', fixed=T)
+d$af <- as.numeric(sapply(infos, '[', 7))
+d$cn <- as.numeric(sapply(infos, '[', 8))
+d$ref <- as.numeric(sapply(infos, '[', 5))
+d$alt <- as.numeric(sapply(infos, '[', 6))
+d$tot <- d$ref+d$alt
+summary(d$tot)
+d$vaf <- d$alt / d$tot
+d <- d[d$cn %in% c(1,2,3),]
+ggplot(data=d, aes(x=vaf, y=..count.., color=as.factor(cn)))+geom_density(position="identity")+theme_bw()
+d$caf <- d$vaf * d$cn
+ggplot(data=d, aes(x=caf, y=..count..))+geom_density(position="identity")+theme_bw()
+
+                                                           
+
+rbinom <- function(mut) {
+  refreads <- as.integer(mut[7])
+  mutreads <- as.integer(mut[8])
+  cn <- as.integer(mut[6])
+  tot <- refreads+mutreads
+  s <- 1/cn
+  sqrt_1_s <- (1-s)**(1/2)
+  sqrt_s <- (s)**(1/2)
+  sqrt_n <- (tot)**(1/2)
+  if (cn==0) {
+    return(0)
+  }
+  #print(paste0('Cn= ', cn))
+  #print(paste0('refreads= ', refreads))
+  #print(paste0('mutreads= ', mutreads))
+  #s - (sqrt(1-s)*sqrt(s))/(sqrt(n)  < x < s + (sqrt(1-s)*sqrt(s))/(sqrt(n)
+  if (mutreads/tot >  s - ((sqrt_1_s*sqrt_s)/sqrt_n) && mutreads/tot <  s + ((sqrt_1_s*sqrt_s)/sqrt_n)) {
+    return(1)
+  } else {
+    return(0)
+  }
+}
+new_binom <- apply(d, 1, rbinom)
+d$bino <- new_binom
+
+db <- d[d$bino ==1,]
+dnb <- d[d$bino ==0,]
+ggplot(data=db, aes(x=caf, y=..count..))+geom_density(position="identity")+theme_bw()
+
+ggplot(data=d, aes(x=caf))+geom_histogram()+theme_bw()
+ggplot(data=db, aes(x=caf))+geom_histogram()+theme_bw()
+ggplot(data=dnb, aes(x=caf))+geom_histogram()+theme_bw()
---
+++ local/src/mpileup_counts.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+if (scalar(@ARGV) != 0) {
+    die "Usage $0 < pseudompileup"
+}
+my $tot = 0;
+my $totmut = 0;
+my $chr = '';
+my $b = 0;
+my $e = 0;
+my $laste = 0;
+my $lastchr = '';
+my $already = 0;
+while (<STDIN>) {
+    chomp;
+    my @line = split("\t", $_);
+    my $bases = $line[5];
+    # we must manage end of chr if we ended it in a non mutated interval
+    # but the management needs to be done depending on how we begin the new chr. ?
+    if ($lastchr ne '' && $line[0] ne $lastchr && $b != 0) {
+        print $lastchr . "\t" . $b . "\t" . $laste . "\n";
+        $already = 1;
+    }
+    if ($bases =~ /[ACGTNacgtn><*]/) {
+        $totmut++;
+        #end of a non mutated interval (or going on with muts)
+        if ($b != 0) {
+            if ($already == 0) {
+                $already = 0;
+                $e = $line[1];
+                print $chr . "\t" . $b . "\t" . $e . "\n";
+            }
+            $chr = '';
+            $b = 0;
+            $e = 0;
+        }
+    } else {
+        if ($b == 0) { # we open a non mutated interval
+            $already = 0;
+            $chr = $line[0];
+            $b = $line[1];
+        } 
+        # if $b !=0 we are extending our non mutated interval
+    }
+    $tot++;
+    $laste = $line[2];
+    $lastchr = $line[0];
+}
+# last interval, we assume it will be on the last seen chr.
+if ($b != 0) {
+    print $lastchr . "\t" . $b . "\t" . $laste . "\n";
+}
+print STDERR "$tot\t$totmut\n";
