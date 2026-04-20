98c6eae8afd8f1e87d66b6dbcb9155251c900b92
Elena Grassi <grassi.e@gmail.com>
2026-04-20 17:56:14 +0200

    Wanderings in SBS8/SBS1

--- local/src/wip_sbs_t0_t1.R
+++ local/src/wip_sbs_t0_t1.R
@@ -39,3 +39,35 @@ pheatmap(data, fontsize_row = 9, fontsize_col=9, show_colnames = TRUE,  cluster_
 data2 <- data[!grepl('-M', rownames(data)),]
 annot_rows2 <- annot_rows[annot_rows$time %in% c(0, 1),]
 pheatmap(data2, fontsize_row = 9, fontsize_col=9, show_colnames = TRUE,  cluster_cols=FALSE,  annotation_row=annot_rows2, color=brewer.pal(9, 'PuBu'))
+
+
+data3 <- data2[,apply(data2, 2, sum) > 1 ]
+pheatmap(data3, fontsize_row = 9, fontsize_col=9, show_colnames = TRUE,  cluster_cols=FALSE,  annotation_row=annot_rows2, color=brewer.pal(9, 'PuBu'))
+
+###################
+dd <- data.frame(ratio=data2[,8] / data2[,1], row.names= rownames(data2))
+dd$time <-  as.factor(unlist(lapply(strsplit(rownames(dd),'-'), function(x){ x[[3]] })))
+dd$model <-  as.factor(unlist(lapply(strsplit(rownames(dd),'-'), function(x){ x[[1]] })))
+dd$clone <-  as.factor(unlist(lapply(strsplit(rownames(dd),'-'), function(x){ x[[2]] })))
+
+# only in vivo T1 for this
+dd <- dd[rownames(dd) != 'CRC1599LM-07-0',]
+
+for (m in unique(dd$model)) {
+  md <- dd[dd$model == m, ]
+  md0 <- md[md$time == 0,]
+  md0$lineage <- NA
+  md0$lineage <- paste0('clone', md0[,'clone'])
+  md1 <- md[md$time == 1,]
+  md1$lineage <- NA
+  for (c in unique(md1$clone)) {
+    ll <- median(md1[md1$clone == c,'ratio'])
+    if (!any(ll ==  md1[md1$clone == c,'ratio'])) {
+      # solo CRC1599PR
+      ll = max(md1[md1$clone == c,'ratio'])
+    }
+    md1[which(md1$clone==c & md1$ratio == ll),]$lineage <- paste0('clone', c)
+  }
+  md <- rbind(md0, md1)
+  print(ggplot(data=md, aes(x=time, y= ratio))+geom_line(data=md[!is.na(md$lineage),], aes(group=lineage, color=clone))+geom_point(aes(color=clone))+theme_bw(base_size = 15)+ggtitle(m))
+}
